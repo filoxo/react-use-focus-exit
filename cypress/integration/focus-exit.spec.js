4785455ca577733749c62bf18b477e2fae7e7453
Carlos Filoteo <carlos.filoteo@podium.com>
2019-11-13 14:13:22 -0700

    Add e2e tests using Cypress

---
+++ cypress/integration/focus-exit.spec.js
@@ -0,0 +1,36 @@
+describe("FocusExit e2e", function() {
+  it("Closes the menu when clicking away from the menu trigger", function() {
+    cy.visit("/");
+    const menuButton = cy.queryByText("Menu Options");
+    menuButton.should("exist");
+    menuButton.click();
+    cy.queryByText("duckduckgo").should("exist");
+    cy.get("body").click();
+    cy.queryByText("duckduckgo").not("exist");
+  });
+
+  it("Closes the menu when tabbing off of menu", function() {
+    cy.visit("/");
+    cy.queryByText("Menu Options").click();
+    cy.queryByText("duckduckgo").should("exist");
+    cy.queryByText("Menu Options").tab();
+    cy.queryByText("duckduckgo").not("exist");
+  });
+
+  it("displays banner when clicking away from the form", () => {
+    cy.visit("/");
+    cy.findByLabelText("Email:").click();
+    cy.findByText("Submit").focus();
+    cy.get("body").click();
+
+    cy.queryByText(/Focus has left the form/i).should("exist");
+  });
+  it("displays banner when tabbing away from the form", () => {
+    cy.visit("/");
+    cy.findByLabelText("Email:").click();
+    cy.findByText("Submit").focus();
+    cy.get("body").focus();
+
+    cy.queryByText(/Focus has left the form/i).should("exist");
+  });
+});

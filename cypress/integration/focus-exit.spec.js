952f56ecf55d90aac4562660b129b63f95ec5dac
Carlos Filoteo <c.filoteo12@gmail.com>
2020-03-21 23:55:09 -0600

    Replace queryByText with findByText

--- cypress/integration/focus-exit.spec.js
+++ cypress/integration/focus-exit.spec.js
@@ -1,20 +1,20 @@
 describe("FocusExit e2e", function() {
   it("Closes the menu when clicking away from the menu trigger", function() {
     cy.visit("/");
-    const menuButton = cy.queryByText("Menu Options");
+    const menuButton = cy.findByText("Menu Options");
     menuButton.should("exist");
     menuButton.click();
-    cy.queryByText("duckduckgo").should("exist");
+    cy.findByText("duckduckgo").should("exist");
     cy.get("body").click();
-    cy.queryByText("duckduckgo").not("exist");
+    cy.findByText("duckduckgo").not("exist");
   });
 
   it("Closes the menu when tabbing off of menu", function() {
     cy.visit("/");
-    cy.queryByText("Menu Options").click();
-    cy.queryByText("duckduckgo").should("exist");
-    cy.queryByText("Menu Options").tab();
-    cy.queryByText("duckduckgo").not("exist");
+    cy.findByText("Menu Options").click();
+    cy.findByText("duckduckgo").should("exist");
+    cy.findByText("Menu Options").tab();
+    cy.findByText("duckduckgo").not("exist");
   });
 
   it("displays banner when clicking away from the form", () => {
@@ -23,7 +23,7 @@ describe("FocusExit e2e", function() {
     cy.findByText("Submit").focus();
     cy.get("body").click();
 
-    cy.queryByText(/Focus has left the form/i).should("exist");
+    cy.findByText(/Focus has left the form/i).should("exist");
   });
   it("displays banner when tabbing away from the form", () => {
     cy.visit("/");
@@ -31,6 +31,6 @@ describe("FocusExit e2e", function() {
     cy.findByText("Submit").focus();
     cy.get("body").click();
 
-    cy.queryByText(/Focus has left the form/i).should("exist");
+    cy.findByText(/Focus has left the form/i).should("exist");
   });
 });
